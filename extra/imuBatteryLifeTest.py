f462ff4b4a75e8d20a7efcac52ddbc38580b4a4c
Rory <rorybennett123@gmail.com>
2022-06-07 11:45:00 +0100

    show imu value updates on screen

--- extra/imuBatteryLifeTest.py
+++ extra/imuBatteryLifeTest.py
@@ -9,6 +9,8 @@ import PySimpleGUI as sg
 from datetime import datetime as dt
 from pathlib import Path
 import witmotion as wm
+from matplotlib.figure import Figure
+import utils as ut
 
 # Location of refresh icon, stored for main program.
 refreshIcon = str(Path().absolute().parent) + '\\icons\\refresh_icon.png'
@@ -27,12 +29,25 @@ class ImuBatterLifeTest:
         self.comPort = 'COM3'
         self.baudRate = 115200
         self.quaternion = None
+        self.acceleration = None
+        self.angle = None
         # Connection state of the IMU.
         self.isConnected = False
+        # Plotting variables: axis, points, lines, fig_agg, and bg set to None until initialised.
+        self.ax = None
+        self.pointData = None
+        self.lineData = None
+        self.fig_agg = None
+        self.bg = None
         # Create main window for display.
         self.window = sg.Window('IMU Battery Tester', self.layout, finalize=True)
+
+        self.createOrientationPlot(c.DEFAULT_AZIMUTH)
+
         # Display loop.
         while True:
+            self.updateIMUValues()
+            self.updateOrientationPlot()
             event, values = self.window.read(0)
             # Close window event (exit).
             if event == sg.WIN_CLOSED:
@@ -49,8 +64,13 @@ class ImuBatterLifeTest:
             # Toggle IMU connection.
             if event == '-BUTTON-IMU-CONNECT-':
                 self.toggleImuConnect()
+            # Set azimuth.
+            if event == '-SLIDER-AZIMUTH-':
+                self.setAzimuth(int(values['-SLIDER-AZIMUTH-']))
+
         # Close IMU connections manually.
         print('Program closing down...')
+
         if self.isConnected:
             self.imu.ser.close()
             self.imu.close()
@@ -84,12 +104,20 @@ class ImuBatterLifeTest:
 
         imuPlotLayout = [
             [sg.Text('IMU Orientation Plot', size=(40, 1), justification='center', font=st.HEADING_FONT)],
-            [sg.Canvas(key='-CANVAS-PLOT-', size=(500, 500))]
+            [sg.Canvas(key='-CANVAS-PLOT-', size=(500, 500))],
+            [sg.Text('Select Azimuth', font=st.DESC_FONT, pad=((0, 0), (15, 0)))],
+            [sg.Slider(key='-SLIDER-AZIMUTH-', range=(0, 360), default_value=c.DEFAULT_AZIMUTH, size=(40, 10),
+                       orientation='h', enable_events=True)],
         ]
 
-        imuRatePlot = [
-            [sg.Text('IMU Return Rate Plot', size=(40, 1), justification='center', font=st.HEADING_FONT)],
-            [sg.Canvas(key='-CANVAS-PLOT-', size=(500, 500))]
+        imuValuesLayout = [
+            [sg.Text('IMU Values', size=(40, 1), justification='center', font=st.HEADING_FONT)],
+            [sg.Text('Acceleration (m/s^2): ', justification='left', font=st.DESC_FONT, size=(20, 1)),
+             sg.Text(key='-TEXT-ACCELERATION-', text='', justification='right', font=st.DESC_FONT, size=(30, 1))],
+            [sg.Text('Quaternion: ', justification='left', font=st.DESC_FONT, size=(20, 1)),
+             sg.Text(key='-TEXT-QUATERNION-', text='', justification='right', font=st.DESC_FONT, size=(30, 1))],
+            [sg.Text('Euler Angles (deg): ', justification='left', font=st.DESC_FONT, size=(20, 1)),
+             sg.Text(key='-TEXT-ANGLE-', text='', justification='right', font=st.DESC_FONT, size=(30, 1))]
         ]
 
         testControlLayout = [
@@ -104,7 +132,7 @@ class ImuBatterLifeTest:
             [sg.Column(imuLayout, element_justification='center', vertical_alignment='top', justification='c')],
             [sg.HSep(pad=((0, 10), (10, 20)))],
             [sg.Column(imuPlotLayout, element_justification='center', vertical_alignment='top'),
-             sg.Column(imuRatePlot, element_justification='center', vertical_alignment='top')],
+             sg.Column(imuValuesLayout, element_justification='center', vertical_alignment='top')],
             [sg.HSep(pad=((0, 10), (10, 20)))],
             [sg.Column(testControlLayout, element_justification='center', vertical_alignment='top', justification='c')]
         ]
@@ -149,6 +177,56 @@ class ImuBatterLifeTest:
         self.window['-COMBO-RETURN-RATE-'].update(disabled=True if not self.isConnected else False)
         self.window['-BUTTON-IMU-CALIBRATE-'].update(disabled=True if not self.isConnected else False)
 
+    def createOrientationPlot(self, azimuth):
+        """
+        Instantiate the initial plotting variables: The Figure and the axis. This is also called when changing
+        the azimuth of the plot as the entire canvas needs to be redrawn.
+
+        Args:
+            azimuth (int): Azimuth angle in degrees.
+        """
+        fig = Figure(figsize=(5, 5), dpi=100)
+        self.ax = fig.add_subplot(111, projection='3d')
+        fig.patch.set_facecolor(sg.DEFAULT_BACKGROUND_COLOR)
+
+        self.ax = ut.initialiseAxis(self.ax, azimuth)
+        self.ax.disable_mouse_rotation()
+
+        self.fig_agg = ut.drawFigure(fig, self.window['-CANVAS-PLOT-'].TKCanvas)
+
+        self.bg = self.fig_agg.copy_from_bbox(self.ax.bbox)
+
+    def updateOrientationPlot(self):
+        """
+        Update the plot to show orientation of the IMU unit.
+        """
+        # Only plot if the IMU is connected, and a quaternion value is available.
+        if self.isConnected and self.quaternion:
+            self.fig_agg.restore_region(self.bg)
+
+            self.ax = ut.plotPointsOnAxis(self.ax, self.quaternion)
+
+            self.fig_agg.blit(self.ax.bbox)
+            self.fig_agg.flush_events()
+
+    def setAzimuth(self, azimuth):
+        """
+        Set the azimuth of the plot to the slider value. This allows for aligning the plot to the user's orientation
+        since the IMU orientation is based on magnetic north. The axis needs to be cleared first, then reinitialised
+        to ensure a clean plot is saved for blit purposes.
+
+        Args:
+            azimuth (int): Azimuth to set the displayed plot to.
+        """
+        # Clear axis.
+        self.ax.cla()
+        # Reinitialise axis.
+        self.ax = ut.initialiseAxis(self.ax, azimuth)
+        # Redraw new axis.
+        self.fig_agg.draw()
+        # Re-save background for blit.
+        self.bg = self.fig_agg.copy_from_bbox(self.ax.bbox)
+
     def imuCallback(self, msg):
         """
         Callback subscribed to the IMU object. Called whenever a new dataset is ready to be read. This callback is
@@ -162,12 +240,27 @@ class ImuBatterLifeTest:
         msg_type = type(msg)
 
         if msg_type is wm.protocol.AccelerationMessage:
-            pass
+            self.acceleration = self.imu.get_acceleration()
         elif msg_type is wm.protocol.QuaternionMessage:
             self.quaternion = self.imu.get_quaternion()
             self.imuTestCounter += 1
         elif msg_type is wm.protocol.AngleMessage:
-            pass
+            self.angle = self.imu.get_angle()
+
+    def updateIMUValues(self):
+        """
+        Update the shown IMU values if they are available.
+        """
+        if self.isConnected:
+            if self.acceleration:
+                self.window['-TEXT-ACCELERATION-'].update(
+                    f'[{self.acceleration[0]:.4f}, {self.acceleration[1]:.4f}, {self.acceleration[2]:.4f}]')
+            if self.quaternion:
+                self.window['-TEXT-QUATERNION-'].update(
+                    f'[{self.quaternion[0]:.4f}, {self.quaternion[1]:.4f}, {self.quaternion[2]:.4f}, '
+                    f'{self.quaternion[3]:.4f}]')
+            if self.angle and self.angle[0]:
+                self.window['-TEXT-ANGLE-'].update(f'[{self.angle[0]:.4f}, {self.angle[1]:.4f}, {self.angle[2]:.4f}]')
 
 
 ImuBatterLifeTest()
