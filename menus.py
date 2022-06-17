a45da5028fedeafbf9ae556a4334a12c635e61df
Rory <rorybennett123@gmail.com>
2022-06-17 14:31:59 +0100

    create layouts for signal menus

--- menus.py
+++ menus.py
@@ -6,12 +6,30 @@ class.
 import constants as c
 
 """
-Video source menu. For selecting the video source and changing its properties.
+Signal source menus. For selecting the video source and changing its properties.
+
+menuSignalDisconnected:     Menu to show when the signal source is not connected. Since there is no signal, the signal
+                            properties cannot be changed.
+menuSignalConnected:        Menu to show when the signal source is connected. Now that the source is connected, the 
+                            source signal can be changed, it can be disconnected, and properties can be changed.
 """
-menuVideoSource = ['Video Source', ['1']]
+menuSignalDisconnected = ['Signal Source', ['Connect to Source::-MENU-SIGNAL-CONNECT-',
+                                            '!Disconnect from Source::-MENU-SIGNAL-DISCONNECT-',
+                                            '---',
+                                            '!Change Signal Dimensions::-MENU-SIGNAL-DIMENSIONS',
+                                            ]
+                          ]
+
+menuSignalConnected = ['Signal Source', ['Change Source::-MENU-SIGNAL-CHANGE-',
+                                         'Disconnect from Source::-MENU-SIGNAL-DISCONNECT-',
+                                         '---',
+                                         'Change Signal Dimensions::-MENU-SIGNAL-DIMENSIONS',
+                                         [f'{i}::-MENU-SIGNAL-DIMENSIONS-' for i in c.COMMON_SIGNAL_DIMENSIONS]
+                                         ]
+                       ]
 
 """
-Imu menus. 
+Imu menus. For controlling the connection to the IMU.
 
 imuMenuDisconnected:    Menu to show when the IMU is not connected. Since the IMU is not connected the return rate 
                         cannot be changed and the acceleration cannot be calibrated, so these options are disabled.
@@ -20,10 +38,12 @@ imuMenuConnected:       Menu to show when the IMU has been connected, enabling r
 """
 menuImuDisconnected = ['IMU', ['Connect::-MENU-IMU-CONNECT-',
                                '---',
-                               '!Set Return Rate', [f'{i}::-MENU-IMU-RATE-' for i in c.IMU_RATE_OPTIONS],
-                               '!Calibrate Acceleration::-MENU-IMU-CALIBRATE-']]
+                               '!Set Return Rate',
+                               '!Calibrate Acceleration::-MENU-IMU-CALIBRATE-']
+                       ]
 
 menuImuConnected = ['IMU', ['Disconnect::-MENU-IMU-DISCONNECT-',
                             '---',
                             'Set Return Rate', [f'{i}::-MENU-IMU-RATE-' for i in c.IMU_RATE_OPTIONS],
-                            'Calibrate Acceleration::-MENU-IMU-CALIBRATE-']]
+                            'Calibrate Acceleration::-MENU-IMU-CALIBRATE-']
+                    ]
