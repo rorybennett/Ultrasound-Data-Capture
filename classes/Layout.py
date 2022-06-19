5396289dc76cb9f875f1df8105e1218513f6080d
rorybennett <rorybennett123@gmail.com>
2022-06-19 15:32:39 +0100

    start adding layout class

---
+++ classes/Layout.py
@@ -0,0 +1,10 @@
+"""
+Class for handling all layouts used in DataCaptureDisplay. This class is intended to clean up the DataCaptureDisplay
+class to make the code more readable. Future layout updates should be easier with the separation.
+"""
+import constants as c
+
+
+class Layout:
+    def __init__(self):
+        pass
