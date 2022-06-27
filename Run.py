dbdd1f122017cbf5265388d64571a02bf69a0d23
Rory <rorybennett123@gmail.com>
2022-06-27 14:16:31 +0100

    Add a todo list

--- Run.py
+++ Run.py
@@ -10,3 +10,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+TODO:
+    -Clear plot when switching between editing and not editing. Just need an IMU to test if this is being implemented
+     correctly.
+    -Consider the top of the frame displayed is not part of the IMU. The output of the Us scanner has a lot of
+     extra information surrounding the image. A square POI is required within the output signal.
+"""
