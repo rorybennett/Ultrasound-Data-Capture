14724da59e004ad8b82cd87fd2f493294c1c054b
Rory <rorybennett123@gmail.com>
2022-06-29 11:56:50 +0100

    add todo

--- Run.py
+++ Run.py
@@ -19,4 +19,5 @@ TODO:
     -Consider the top of the frame displayed is not part of the IMU. The output of the Us scanner has a lot of
      extra information surrounding the image. A square POI is required within the output signal.
     -Make mainWindow timeout a variable that is None when signal is disconnected, and 1 when it is connected.
+    -split utils into multiple utils
 """
