9cd3abc2ae24dd1ca34d1d22b076df41525799b2
Rory <rorybennett123@gmail.com>
2022-09-11 14:47:14 +0000

    clear todo

--- Run.py
+++ Run.py
@@ -13,11 +13,4 @@ if __name__ == '__main__':
 
 
 """
-TODO:
-    -Clear plot when switching between editing and not editing. Just need an IMU to test if this is being implemented
-     correctly.
-    -Consider the top of the frame displayed is not part of the IMU. The output of the Us scanner has a lot of
-     extra information surrounding the image. A square POI is required within the output signal.
-    -Make mainWindow timeout a variable that is None when signal is disconnected, and 1 when it is connected.
-    -split utils into multiple utils
 """
